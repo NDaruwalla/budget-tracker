35aed07ffa7d31a78de47f51b3e8a01cc716d24a
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-07-15 11:42:53 -0400

    deploy corrections

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+//let mongoconnect = process.env.MONGODB_URI;
 
-const PORT = 3087;
-
+const PORT = process.env.PORT || 3045;
+//const PORT = 8080;
 const app = express();
 
 app.use(logger("dev"));
@@ -16,13 +17,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+  //process.env.MONGOLAB_URI,
+  //process.env.MONGODB_URI || "mongodb://localhost/budget",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-});
+  });
+  //console.log(mongoconnect);
 
 // routes
 app.use(require("./routes/api.js"));
